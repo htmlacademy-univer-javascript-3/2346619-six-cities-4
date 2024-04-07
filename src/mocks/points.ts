703bc02e64ea0b3dd9fd4c96f616ffb1ec067df0
Flay98 <jancovalenko@yandex.ru>
2024-04-08 01:15:05 +0300

    Больше подробностей (часть 1)

---
+++ src/mocks/points.ts
@@ -0,0 +1,20 @@
+import {Point} from '../types/point';
+
+export const points: Point[] = [
+  {
+    latitude: 52.3909553943508,
+    longitude: 4.85309666406198
+  },
+  {
+    latitude: 52.3609553943508,
+    longitude: 4.85309666406198
+  },
+  {
+    latitude: 52.3909553943508,
+    longitude: 4.929309666406198
+  },
+  {
+    latitude: 52.3809553943508,
+    longitude: 4.939309666406198
+  },
+];
