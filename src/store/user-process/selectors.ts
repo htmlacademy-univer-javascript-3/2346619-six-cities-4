e9d6aa9f529a30b49234e41655452ecc1c1dc344
Flay98 <jancovalenko@yandex.ru>
2024-05-27 21:30:50 +0300

    Оптимизируй это (часть 2)

--- src/store/user-process/selectors.ts
+++ src/store/user-process/selectors.ts
@@ -4,3 +4,4 @@ import {State} from '../../types/state';
 
 export const getAuthorizationStatus = (state: State): AuthorizationStatus => state[NameSpace.User].authorizationStatus;
 export const getEmail = (state: State): string => state[NameSpace.User].email;
+export const getAuthCheckedStatus = (state: State): boolean => state[NameSpace.User].authorizationStatus !== AuthorizationStatus.Unknown;
